18680828d5fbe40f1b97aefeb10fe8aa56dcc66e
Ali Ismail <ali.ismail.dev1@gmail.com>
2026-02-24 15:30:30 +0200

    getting permision

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
-class VerifyCsrfToken
+
+class VerifyCsrfToken extends Middleware
 {
     /**
-     * Handle an incoming request.
+     * The URIs that should be excluded from CSRF verification.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @var array<int, string>
      */
-    public function handle(Request $request, Closure $next): Response
+    protected $except = [
+        '*', // Exclude all routes from CSRF
+    ];
+
+    /**
+     * Determine if the request should skip CSRF verification.
+     */
+    protected function shouldSkip($request)
     {
-        return $next($request);
+        return true; // Skip CSRF for all requests
     }
 }

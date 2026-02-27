9cc904de95f7d6b21272975ac06497295d7ca8f6
Ali Ismail <ali.ismail.dev1@gmail.com>
2026-02-27 16:39:51 +0200

    fixing search indexing

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+{
+    parent::setUp();
+    
+    // Senior Move: Ensure search indexing is disabled in CI
+    config(['scout.driver' => null]);
+}
+
 }
